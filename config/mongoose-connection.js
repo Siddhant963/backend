315784264e0319997ae75da23d4d51d1a18369d0
Siddhantwork099 <siddhantwork867@gmail.com>
2025-01-23 02:59:28 +0530

    env setup

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const config = require('config');
+const dugr = require('debug')("development:mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/Two_cups')
+mongoose.connect(`${config.get("MONGODB_URI")}/Two_cups`)
 .then(function(){ 
-     console.log("Database connected");
+     dugr("Database connected");
 })
 .catch(function(err){ 
-  console.log(err);
+  dugr(err);
   
 })
 module.exports = mongoose.connection;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+// console.log(process.NODE_ENV);
+
 router.get("/" , (req,res)=>{ 
      res.send("Welcome to the user router");
 });
